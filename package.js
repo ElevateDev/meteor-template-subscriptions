095a808394b7f2ccb6d9211737b9bdc253fd526a
Jimmie Butler <jimmiebtlr@gmail.com>
2015-01-21 03:22:57 -0600

    Bump to version 1.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'elevatedevdesign:template-subscriptions',
   summary: 'Template based subscriptions for meteor',
-  version: '0.0.1-rc.3',
+  version: '0.0.1',
   git: 'https://github.com/ElevateDevelopmentAndDesign/meteor-template-subscriptions'
 });
 
